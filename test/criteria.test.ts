eedb335d8cbbf1b688dd63d3512a68e75dcf7917
HarryDang <danghuuphuc001@gmail.com>
2024-03-11 07:44:02 +0700

    change token NPM_AUTH_TOKEN

--- test/criteria.test.ts
+++ test/criteria.test.ts
@@ -1224,7 +1224,7 @@ describe("testing for function imageValidation: ", () => {
         imageValidation(images, "string input");
       }).toThrow();
     });
-    test("value of images array is not object data type welcome", () => {
+    test("value of images array is not object data type ", () => {
       expect(() => {
         // @ts-ignore
         imageValidation(["string input"], true);
